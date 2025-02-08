a69b9ac957ed9321646cc90778b36af00e1a75d9
ryo-i1 <bluestone2853@gmail.com>
2025-02-09 02:29:34 +0900

    abc392 b

--- abc392/b.py
+++ abc392/b.py
@@ -1,7 +1,7 @@
 N, M = map(int, input().split())
 A = [int(x) for x in input().split()]
 
-X = set(range(1, N + 1)) - set(A)
+X = [i for i in range(1, N + 1) if i not in A]
 
 print(N - M)
 print(*X)
